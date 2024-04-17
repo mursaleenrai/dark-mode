fc8ba7a8a10497566e9f402c9b720ab1942bac8a
mursaleenrai <raimursaleen14@gmail.com>
2024-04-17 14:39:47 +0500

    dark mode

---
+++ mui/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const Home = () => {
+    return (
+        <div>
+            <Typography variant='h1' bgcolor={'secondary.light'}>
+                Home
+            </Typography>
+        </div>
+    )
+}
+
+export default Home
